c1b25fb06cde9a99da4410f198fc06e009257602
Giranda22-Git <aloxaeee@bk.ru>
2021-06-29 10:51:03 +0600

    fix

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,8 +7,8 @@ const serverData = require('./staticData/mountedData.js')
 
 const app = express()
 
-app.use(bodyParser.json({limit: '50mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
 	res.contentType('application/json')
 	next()
