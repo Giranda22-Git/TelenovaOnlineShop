b741975f212a5336d68dcc49eff06708e468b88d
Giranda22-Git <aloxaeee@bk.ru>
2021-09-05 20:43:40 +0600

    Update storage.js

--- backEnd/endPoints/storage.js
+++ backEnd/endPoints/storage.js
@@ -748,7 +748,7 @@ wsClient.on('connection', async (client, data) => {
           }
           else if (key === "priceRange") {
             shop = shop.filter(element => {
-              return element.offerData.price >= Number(data.filters[key][0]) && element.offerData.price <= Number(data.filters[key][1])
+              return element.salePrice >= Number(data.filters[key][0]) && element.salePrice <= Number(data.filters[key][1])
             })
           }
           else if (key === "switchers") {
