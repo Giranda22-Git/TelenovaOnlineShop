c92c6f2ee313976b5aece439cb23dba836d416cd
Giranda22-Git <aloxaeee@bk.ru>
2021-09-13 18:29:23 +0600

    Update storage.js

--- backEnd/endPoints/storage.js
+++ backEnd/endPoints/storage.js
@@ -1118,7 +1118,7 @@ function regExpGenerate (productName, str, recurseStr, iteration) {
   result.sort((a, b) => a.result.length - b.result.length)
 
   if (str === 'клонка') {
-    console.log('regExpRes: ', result)
+    console.log('regExpRes: ', str, result)
   }
 
   if (result.length === 0) {
